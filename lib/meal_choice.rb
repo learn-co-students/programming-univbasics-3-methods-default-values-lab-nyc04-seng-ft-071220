c4585f5cded39587ce06fba0a80478532f9fb58d
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-16 20:23:25 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,11 @@
+def meal_choice (veg1,veg2,protein = 'tofu')
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  # must return the last puts inorder to get it all r/t
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+
+
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
